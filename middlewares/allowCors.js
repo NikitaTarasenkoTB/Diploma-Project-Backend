dbadc8bb5628649c5b95afc7bfd0b6013cdae0c5
Nikita Tarasenko <skaerlll@gmail.com>
2020-12-03 23:07:30 +0300

    cors issues 5

--- middlewares/allowCors.js
+++
@@ -1,8 +0,0 @@
-module.exports = (request, response, next) => {
-  const allowedCors = ['http://localhost:8080']
-  const { origin } = request.headers;
-  if (allowedCors.includes(origin)) {
-    response.header('Access-Control-Allow-Origin', origin);
-  }
-  next();
-}

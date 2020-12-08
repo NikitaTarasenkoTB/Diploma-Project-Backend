64de91993fbf37a8a38de26ba0afc8ece3b38918
Nikita Tarasenko <skaerlll@gmail.com>
2020-12-09 00:51:47 +0300

    rate limiter fixes

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,6 +2,6 @@ const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 100,
+  max: 1000,
   message: 'Слишком много запросов с данного IP. Поажалуйста повторите попытку позже.',
 });
